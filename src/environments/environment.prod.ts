831be5ed25a96cdae2446aec6af1b2367ae4a69e
Khien.Pham <phamkhien@hotmail.com>
2021-03-11 20:31:58 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mo-backend.herokuapp.com/api/',
+  apiUrl: 'https://admin.thicongchuc.org/api/',
   googleClientId:
-    '543678162369-go3u1vv05h1pp9odsh9uc7i7v0ep1a3l.apps.googleusercontent.com', // secret: jsFL-97-i4mxkQFHMEBtUUJc
-  facebookClientId: '486122019055775',
+    '543678162369-go3u1vv05h1pp9odsh9uc7i7v0ep1a3l.apps.googleusercontent.com',
+  facebookClientId: '424314148864878',
 };
