d662ff56aacb823f8bd066c953ebdef7210e17dd
Khien.Pham <phamkhien@hotmail.com>
2021-03-21 00:20:09 +0700

    Update

--- src/app/@containers/subject/subject.module.ts
+++ src/app/@containers/subject/subject.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
 import { SubjectRoutingModule } from './subject-routing.module';
 import { SubjectDetailComponent } from '@containers/subject/subject-detail/subject-detail.component';
+import { PostModule } from '@components/post/post.module';
 
 @NgModule({
   declarations: [SubjectDetailComponent],
-  imports: [SharedModule, SubjectRoutingModule],
+  imports: [SharedModule, SubjectRoutingModule, PostModule],
 })
 export class SubjectModule {}
