8d80fe0dee724d0d301a5404791851f66592d62d
Khien.Pham <phamkhien@hotmail.com>
2021-04-18 14:02:29 +0700

    Update

--- src/app/containers/post/post-item.component.ts
+++ src/app/containers/post/post-item.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { PostModel } from '@core/models/post.model';
 import { PostService } from '@core/services/post.service';
 import { Helper } from '@core/helpers/helper';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
 export class PostItemComponent implements OnInit {
   postId: number;
-  post: PostModel;
+  postTitle: string;
+  postContent: SafeHtml;
 
-  constructor(private route: ActivatedRoute, public postService: PostService) {}
+  constructor(
+    private route: ActivatedRoute,
+    public postService: PostService,
+    private sanitizer: DomSanitizer,
+  ) {}
 
   ngOnInit(): void {
     this.postId = Helper.getId(<string>this.route.snapshot.paramMap.get('id'));
 
     this.postService.getById(this.postId).subscribe((post: PostModel) => {
-      this.post = post;
+      this.postTitle = post.title;
+      this.postContent = this.sanitizer.bypassSecurityTrustHtml(post.content);
     });
   }
 }
