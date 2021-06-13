96e17ecc610fcc8358ed7f3c18d213f8b7bd2d32
Khien.Pham <phamkhien@hotmail.com>
2021-06-13 13:37:47 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,9 @@ import { BreadcrumbModule } from '@components/breadcrumb/breadcrumb.module';
     SharedModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:3000',
     }),
   ],
   providers: [
